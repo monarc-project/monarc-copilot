fca1da9d488a27bfe499c5cf0c470898056a66c6
Ruslan Baidan <ruslan.baidan@nc3.lu>
2026-04-23 12:45:34 +0200

    Fixed the config path.

--- Module.php
+++ Module.php
@@ -6,6 +6,6 @@ class Module
 {
     public function getConfig(): array
     {
-        return include dirname(__DIR__) . '/config/module.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
 }
